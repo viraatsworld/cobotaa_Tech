33c8d43d8cf19033a80ffd38e5438f7ba8de8696
Viraat Saaran <viraat.saaran@ipa.fraunhofer.de>
2026-03-12 16:42:28 +0100

    Add curobo-Moveit Setup

---
+++ techtory_cobotta_bringup/launch/isaac_techtory_cobotta_bringup.py
@@ -0,0 +1,270 @@
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import (
+    Command,
+    FindExecutable,
+    LaunchConfiguration,
+    PathJoinSubstitution,
+)
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterFile, ParameterValue
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    declared_arguments = []
+
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "model",
+            default_value="cvrb0609",
+            description="Type/series of used denso robot.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "send_format",
+            default_value="288",
+            description="Data format for sending commands to the robot.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "recv_format",
+            default_value="292",
+            description="Data format for receiving robot status.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "bcap_slave_control_cycle_msec",
+            default_value="8.0",
+            description="Control frequency.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "ip_address",
+            default_value="192.168.0.2",
+            description="IP address by which the robot can be reached.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "description_package",
+            default_value="techtory_cvrb0609_workcell_description",
+            description="Description package with robot URDF/XACRO files.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "description_file",
+            default_value="urdf/techtory_cvrb0609_workcell.urdf.xacro",
+            description="URDF/XACRO description file.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "namespace",
+            default_value="cobotta_pro_",
+            description="Prefix for robot joints.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "controllers_file",
+            default_value="config/controller.yaml",
+            description="YAML file with controllers configuration.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "robot_controller_pkg",
+            default_value="techtory_cobotta_bringup",
+            description="Package that contains controllers_file.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "robot_controller",
+            default_value="denso_joint_trajectory_controller",
+            description="Robot controller to spawn.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "joint_state_broadcaster",
+            default_value="denso_joint_state_broadcaster",
+            description="Joint state broadcaster controller name.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument("launch_rviz", default_value="true", description="Launch RViz2.")
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "sim",
+            default_value="true",
+            description="Start robot with fake hardware mirroring command to its states.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "use_mock_hardware",
+            default_value="false",
+            description="Use mock ros2_control hardware plugin.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "verbose",
+            default_value="false",
+            description="Print out additional debug information.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "cvrb_prefix",
+            default_value="cobotta_pro_",
+            description="Prefix used for robot links in workcell xacro.",
+        )
+    )
+
+    denso_robot_model = LaunchConfiguration("model")
+    ip_address = LaunchConfiguration("ip_address")
+    send_format = LaunchConfiguration("send_format")
+    recv_format = LaunchConfiguration("recv_format")
+    bcap_slave_control_cycle_msec = LaunchConfiguration("bcap_slave_control_cycle_msec")
+    description_package = LaunchConfiguration("description_package")
+    description_file = LaunchConfiguration("description_file")
+    namespace = LaunchConfiguration("namespace")
+    controllers_file = LaunchConfiguration("controllers_file")
+    robot_controller_pkg = LaunchConfiguration("robot_controller_pkg")
+    robot_controller = LaunchConfiguration("robot_controller")
+    joint_state_broadcaster = LaunchConfiguration("joint_state_broadcaster")
+    launch_rviz = LaunchConfiguration("launch_rviz")
+    sim = LaunchConfiguration("sim")
+    use_mock_hardware = LaunchConfiguration("use_mock_hardware")
+    verbose = LaunchConfiguration("verbose")
+    cvrb_prefix = LaunchConfiguration("cvrb_prefix")
+
+    denso_robot_core_pkg = get_package_share_directory("denso_robot_core")
+    denso_robot_control_parameters = {
+        "denso_bcap_slave_control_cycle_msec": bcap_slave_control_cycle_msec,
+        "denso_config_file": PathJoinSubstitution([denso_robot_core_pkg, "config", "config.xml"]),
+    }
+
+    robot_description_content = ParameterValue(
+        Command(
+            [
+                PathJoinSubstitution([FindExecutable(name="xacro")]),
+                " ",
+                PathJoinSubstitution([FindPackageShare(description_package), description_file]),
+                " ",
+                "model:=",
+                denso_robot_model,
+                " ",
+                "namespace:=",
+                namespace,
+                " ",
+                "cvrb_prefix:=",
+                cvrb_prefix,
+                " ",
+                "ip_address:=",
+                ip_address,
+                " ",
+                "send_format:=",
+                send_format,
+                " ",
+                "recv_format:=",
+                recv_format,
+                " ",
+                "verbose:=",
+                verbose,
+                " ",
+                "sim:=",
+                sim,
+                " ",
+                "use_mock_hardware:=",
+                use_mock_hardware,
+                " ",
+            ]
+        ),
+        value_type=str,
+    )
+    robot_description = {"robot_description": robot_description_content}
+
+    robot_controllers = PathJoinSubstitution(
+        [
+            FindPackageShare(robot_controller_pkg),
+            controllers_file,
+        ]
+    )
+
+    control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[
+            robot_description,
+            ParameterFile(robot_controllers, allow_substs=True),
+            denso_robot_control_parameters,
+        ],
+        output={"stdout": "screen", "stderr": "screen"},
+    )
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="screen",
+        parameters=[robot_description],
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[joint_state_broadcaster, "--controller-manager", "/controller_manager"],
+    )
+
+    robot_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[robot_controller, "--controller-manager", "/controller_manager"],
+    )
+
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        condition=IfCondition(launch_rviz),
+        output="screen",
+        parameters=[robot_description],
+    )
+
+    move_group_node = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                PathJoinSubstitution(
+                    [
+                        FindPackageShare("techtory_cobotta_moveit"),
+                        "launch",
+                        "move_group.launch.py",
+                    ]
+                )
+            ]
+        )
+    )
+
+
+    return LaunchDescription(
+        declared_arguments
+        + [
+            control_node,
+            robot_state_publisher_node,
+            joint_state_broadcaster_spawner,
+            robot_controller_spawner,
+            rviz_node,
+            move_group_node,
+        ]
+    )
