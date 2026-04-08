7f153d5c6b429c5a247ae055a461d3700ddd4421
Viraat Saaran <viraat.saaran@ipa.fraunhofer.de>
2026-04-08 15:58:10 +0200

    Add Cumotion with Cartesian Path

--- techtory_cobotta_bringup/launch/techtory_cobotta_hw_bringup.launch.py
+++ techtory_cobotta_bringup/launch/techtory_cobotta_hw_bringup.launch.py
@@ -1,3 +1,5 @@
+import yaml
+
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
@@ -12,6 +14,8 @@ from launch.substitutions import (
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterFile, ParameterValue
 from launch_ros.substitutions import FindPackageShare
+import os
+from moveit_configs_utils import MoveItConfigsBuilder
 
 
 def generate_launch_description():
@@ -82,6 +86,13 @@ def generate_launch_description():
     )
     declared_arguments.append(
         DeclareLaunchArgument(
+            "kinematics_file",
+            default_value="config/kinematics.yaml",
+            description="YAML file with kinematics configuration.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
             "robot_controller_pkg",
             default_value="techtory_cobotta_bringup",
             description="Package that contains controllers_file.",
@@ -114,7 +125,7 @@ def generate_launch_description():
     declared_arguments.append(
         DeclareLaunchArgument(
             "use_mock_hardware",
-            default_value="true",
+            default_value="false",
             description="Use mock ros2_control hardware plugin.",
         )
     )
@@ -142,6 +153,7 @@ def generate_launch_description():
     description_file = LaunchConfiguration("description_file")
     namespace = LaunchConfiguration("namespace")
     controllers_file = LaunchConfiguration("controllers_file")
+    kinematics_file = LaunchConfiguration("kinematics_file")
     robot_controller_pkg = LaunchConfiguration("robot_controller_pkg")
     robot_controller = LaunchConfiguration("robot_controller")
     joint_state_broadcaster = LaunchConfiguration("joint_state_broadcaster")
@@ -156,7 +168,13 @@ def generate_launch_description():
         "denso_bcap_slave_control_cycle_msec": bcap_slave_control_cycle_msec,
         "denso_config_file": PathJoinSubstitution([denso_robot_core_pkg, "config", "config.xml"]),
     }
+    
+    #MOveIt Configurations
+    moveit_config = MoveItConfigsBuilder("techtory_demo_description", package_name="techtory_cobotta_moveit"
+    ).planning_pipelines(pipelines=["ompl", "pilz_industrial_motion_planner", "isaac_ros_cumotion"]                
+      ).to_moveit_configs()
 
+    #use_sim_time = {"use_sim_time": True} 
     robot_description_content = ParameterValue(
         Command(
             [
@@ -197,11 +215,63 @@ def generate_launch_description():
     )
     robot_description = {"robot_description": robot_description_content}
 
+
+    kinematics_yaml_path = os.path.join(
+        get_package_share_directory("techtory_cobotta_moveit"),
+        "config",
+        "kinematics.yaml"
+    )
+    with open(kinematics_yaml_path, "r") as f:
+        kinematics_yaml = yaml.safe_load(f)
+
+
+    robot_description_kinematics = {"robot_description_kinematics": kinematics_yaml}
+
     robot_controllers = PathJoinSubstitution(
         [
             FindPackageShare(robot_controller_pkg),
             controllers_file,
         ]
+    )
+    #Joint Limits 
+    joint_limits_yaml_path = os.path.join(
+    get_package_share_directory("techtory_cobotta_moveit"),
+    "config",
+    "joint_limits.yaml"
+)
+    with open(joint_limits_yaml_path, "r") as f:
+        joint_limits_yaml = yaml.safe_load(f)
+    robot_description_planning = {"robot_description_planning": joint_limits_yaml}
+
+    #Robot Semantic Description
+    srdf_path = os.path.join(
+        get_package_share_directory("techtory_cobotta_moveit"),
+        "config",
+        "techtory_demo_description.srdf"
+    )
+    with open(srdf_path, "r") as f:
+        srdf_content = f.read()
+    robot_description_semantic = {"robot_description_semantic": srdf_content}
+
+
+    #Plannig Pipelines
+#     ompl_yaml_path = os.path.join(
+#     get_package_share_directory("techtory_cobotta_moveit"),
+#     "config",
+#     "ompl_planning.yaml"
+# )
+#     with open(ompl_yaml_path, "r") as f:
+#         ompl_yaml = yaml.safe_load(f)
+#     planning_pipelines = {"ompl": ompl_yaml}
+
+
+        # Static TF
+    world2robot_tf_node = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='static_transform_publisher',
+        output='log',
+        arguments=['--frame-id', 'world', '--child-frame-id', 'cobotta_pro_tool0'],
     )
 
     control_node = Node(
@@ -211,6 +281,7 @@ def generate_launch_description():
             robot_description,
             ParameterFile(robot_controllers, allow_substs=True),
             denso_robot_control_parameters,
+            
         ],
         output={"stdout": "screen", "stderr": "screen"},
     )
@@ -220,6 +291,7 @@ def generate_launch_description():
         executable="robot_state_publisher",
         output="screen",
         parameters=[robot_description],
+        
     )
 
     joint_state_broadcaster_spawner = Node(
@@ -239,7 +311,13 @@ def generate_launch_description():
         executable="rviz2",
         condition=IfCondition(launch_rviz),
         output="screen",
-        parameters=[robot_description],
+        parameters=[robot_description, 
+                    robot_description_kinematics,
+                    robot_description_planning,
+                    robot_description_semantic,
+                    moveit_config.planning_pipelines,
+
+                    ],
     )
 
     move_group_node = IncludeLaunchDescription(
@@ -253,9 +331,8 @@ def generate_launch_description():
                     ]
                 )
             ]
-        )
+        ), launch_arguments={"use_sim_time": "true"}.items()
     )
-
 
     return LaunchDescription(
         declared_arguments
@@ -264,7 +341,9 @@ def generate_launch_description():
             robot_state_publisher_node,
             joint_state_broadcaster_spawner,
             robot_controller_spawner,
+            world2robot_tf_node,
             rviz_node,
             move_group_node,
+
         ]
     )
--- techtory_cobotta_moveit/launch/move_group.launch.py
+++ techtory_cobotta_moveit/launch/move_group.launch.py
@@ -1,14 +1,19 @@
 from pathlib import Path
-
+from launch.actions import DeclareLaunchArgument
 from ament_index_python.packages import get_package_share_directory
 from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_move_group_launch
+from moveit_configs_utils.launches import LaunchDescription, generate_move_group_launch
 
 def generate_launch_description():
 
     moveit_pkg_share = Path(get_package_share_directory("techtory_cobotta_moveit"))
     description_pkg_share= Path(get_package_share_directory("techtory_cobotta_workcell_description"))
-
+        # Declare use_sim_time argument
+    use_sim_time_arg = DeclareLaunchArgument(
+        "use_sim_time",
+        default_value="false",
+        description="Use simulation time"
+    )
     moveit_config = MoveItConfigsBuilder("techtory_demo_description", package_name="techtory_cobotta_moveit"
     ).robot_description(
             file_path=str(
@@ -26,4 +31,8 @@ def generate_launch_description():
         ).joint_limits(
             file_path=str(moveit_pkg_share / "config" / "joint_limits.yaml")
         ).to_moveit_configs()
-    return generate_move_group_launch(moveit_config)
+    move_group_launch = generate_move_group_launch(moveit_config)
+    
+    return LaunchDescription(
+        [use_sim_time_arg] + move_group_launch.entities
+        )
